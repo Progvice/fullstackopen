32b8d8545293f3bec509bb58e4dea5ed5b9029b3
Glafuski <glafuski@gmail.com>
2023-11-12 23:16:15 +0200

    Made anekdote step2

--- osa1/anekdootit/part1/src/App.jsx
+++ osa1/anekdootit/part1/src/App.jsx
@@ -4,11 +4,27 @@ import anecdotes from './consts/Anecdotes';
 
 function App() {
   const [selected, setSelected] = useState(0);
+  const [scoreList, setScoreList] = useState([]);
+
+  const generateScores = () => {
+    const list = [];
+    for(let counter = 0; counter < anecdotes.length; counter++) {
+      list.push(0);
+    }
+    setScoreList(list);
+  }
+  scoreList.length === 0 ? generateScores() : null;
   return (
     <>
       <button onClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))}>Generate anecdote</button>
       <div>
         {anecdotes[selected]}
+        <button onClick={() => {
+          let updatedList = [...scoreList];
+          updatedList[selected] = updatedList[selected] + 1;
+          setScoreList(updatedList);
+        }}>Vote</button>
+        <p>Has {scoreList[selected]} votes</p>
       </div>
     </>
   )
