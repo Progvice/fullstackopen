eec944dbbad0ef721e3ff98dcf5ece6f5b77ff3e
Glafuski <glafuski@gmail.com>
2023-11-12 23:29:33 +0200

    Made anecdote step3

--- osa1/anekdootit/part1/src/App.jsx
+++ osa1/anekdootit/part1/src/App.jsx
@@ -2,6 +2,12 @@ import {useState} from 'react';
 import './App.css'
 import anecdotes from './consts/Anecdotes';
 
+const findBiggestValue = (list) => {
+  let maxValue = Math.max(...list);
+  let i = list.indexOf(maxValue);
+  return i;
+}
+
 function App() {
   const [selected, setSelected] = useState(0);
   const [scoreList, setScoreList] = useState([]);
@@ -16,7 +22,8 @@ function App() {
   scoreList.length === 0 ? generateScores() : null;
   return (
     <>
-      <button onClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))}>Generate anecdote</button>
+    <h1>Anecdote of the day</h1>
+      <button onClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))}>Random anecdotes</button>
       <div>
         {anecdotes[selected]}
         <button onClick={() => {
@@ -25,6 +32,11 @@ function App() {
           setScoreList(updatedList);
         }}>Vote</button>
         <p>Has {scoreList[selected]} votes</p>
+      </div>
+      <div>
+        <h1>Anecdote with most votes</h1>
+        <p>{anecdotes[findBiggestValue(scoreList)]}</p>
+        <p>Has {scoreList[findBiggestValue(scoreList)]} votes</p>
       </div>
     </>
   )
