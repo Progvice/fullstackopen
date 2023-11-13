c75c64fb68d58e911788e97cf4c64e60c86545e8
Glafuski <glafuski@gmail.com>
2023-11-13 23:31:10 +0200

    Made puhelinluettelo step 7, 8, 9 and 10

--- osa2/puhelinluettelo/part1/src/App.jsx
+++ osa2/puhelinluettelo/part1/src/App.jsx
@@ -3,10 +3,7 @@ import './App.css'
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
-import axios from 'axios';
-
-const personsURL = 'http://localhost:3001/persons';
-
+import {getAll, create, update} from './services/persons';
 function App() {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
@@ -14,24 +11,49 @@ function App() {
   const [searchName, setSearchName] = useState('');
 
   useEffect(() => {
-    axios
-    .get(personsURL)
+    getAll()
     .then(res => {
       setPersons(res.data);
     })
     .catch(err => console.log(err)); 
   }, []);
 
+
+  const updateAndFetch = (personDetails) => {
+    if(!window.confirm('This person already exists. Do you want to update this persons information?')) {
+      return;
+    }
+    update(personDetails.id, {name: personDetails.name, phonenumber: newPhonenumber, id: personDetails.id})
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+
+    getAll()
+    .then(res => {
+      setPersons(res.data);
+    })
+    .catch(err => console.log(err)); 
+  }
+
   const addName = (e) => {
     e.preventDefault();
-    if(persons.some(person => person.name === newName)) {
+    if(persons.some(person => person.name === newName && person.phonenumber === newPhonenumber)) {
       alert(`${newName} is already added to phonebook`);
+      return;
+    }
+    const personDetails = persons.find(person => person.name === newName && person.phonenumber !== newPhonenumber);
+    console.log(personDetails);
+    if(personDetails) {
+      updateAndFetch(personDetails);
       return;
     }
     const newPerson = {name: newName, phonenumber: newPhonenumber};
     const personsCopy = [...persons];
     personsCopy.push(newPerson);
     setPersons(personsCopy);
+
+    create(newPerson)
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
   }
 
   return (
@@ -40,7 +62,7 @@ function App() {
       <PersonForm addName={addName} newName={newName} setNewName={setNewName} newPhonenumber={newPhonenumber} setNewPhonenumber={setNewPhonenumber}/>
       <Filter searchName={searchName} persons={persons} setSearchName={setSearchName}/>
       <h2>Numbers</h2>
-      <Persons searchName={searchName} persons={persons} />
+      <Persons searchName={searchName} persons={persons} setPersons={setPersons} />
     </div>
   )
 }
--- osa2/puhelinluettelo/part1/src/components/Persons.jsx
+++ osa2/puhelinluettelo/part1/src/components/Persons.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
+import {deletePerson, getAll} from '../services/persons';
 const Persons = (props) => {
-    const {searchName, persons} = props;
+    const {searchName, persons, setPersons} = props;
+
+    const sendDelete = (id) => {
+        if(!window.confirm('Do you really want to remove this person from phonebook?')) {
+            return;
+        }
+        deletePerson(id);
+        getAll()
+        .then(res => {
+            setPersons(res.data);
+        });
+    }
+
     return (
         <table>
         <tbody>
           <tr>
             <th>Name</th>
             <th>Phonenumber</th>
+            <th>Action</th>
           </tr>
           {searchName.length === 0 ? persons.map((person) => (
-              <tr key={person.name}>
+              <tr key={person.id}>
                 <td>{person.name}</td>
                 <td>{person.phonenumber}</td>
+                <td>
+                    <button onClick={() => sendDelete(person.id)}>Remove</button>
+                </td>
               </tr>
           )) : null}
           {searchName.length > 0 ? 
@@ -19,9 +36,12 @@ const Persons = (props) => {
               const trimmedName = person.name.trim().toLowerCase();
               if(trimmedName.includes(searchName.trim().toLowerCase())) {
                 return (
-                  <tr key={person.name}>
+                  <tr key={person.id}>
                     <td>{person.name}</td>
                     <td>{person.phonenumber}</td>
+                    <td>
+                        <button onClick={() => sendDelete(person.id)}>Remove</button>
+                    </td>
                   </tr>
                 )
               }
